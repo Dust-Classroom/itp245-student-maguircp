719aa1e133c68f2274cb51feb8d216100a378d6a
Ciaran Maguire <maguircp@gmail.com>
2020-11-30 07:25:26 -0500

    revised assembly version/ updated doc log

--- ITP245/Properties/AssemblyInfo.cs
+++ ITP245/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2020.11.09.1")]
-[assembly: AssemblyFileVersion("2020.11.09.1")]
+[assembly: AssemblyVersion("2020.11.29.1")]
+[assembly: AssemblyFileVersion("2020.11.29.1")]
